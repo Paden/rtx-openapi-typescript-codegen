1c49e96ff59b189630f094419a290ef3b094500b
Paden <padenportillo@gmail.com>
2020-03-12 13:26:56 -0500

    Remove any from index.ts

--- src/utils/getModelNames.ts
+++ src/utils/getModelNames.ts
@@ -2,5 +2,8 @@ import { Model } from '../client/interfaces/Model';
 import { sort } from './sort';
 
 export function getModelNames(models: Model[]): string[] {
-    return models.map(model => model.name).sort(sort);
+    return models
+        .map(model => model.name)
+        .filter(modelName => modelName === 'any')
+        .sort(sort);
 }
